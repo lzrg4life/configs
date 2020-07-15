d3353f9f70d5869371c7336aaec4999ce0deee1f
Willis Stearns <lzrg4life@gmail.com>
2020-07-15 12:34:18 -0400

    Add PowerShell profile

---
+++ powershell/profile.ps1
@@ -0,0 +1,43 @@
+clear
+
+# Turn off the error sound
+Set-PSReadLineOption -BellStyle None
+
+# Show all possible completions on tab
+Set-PSReadlineOption -EditMode Emacs
+
+# Set a custom prompt
+function prompt {
+  Write-Output "`r`n$(Get-Location)`r`n$ "
+}
+
+New-Alias -name "v" "nvim.exe"
+New-Alias -name "touch" "New-Item"
+
+$nvprofiledir = "$home\AppData\Local\nvim"
+$nvprofile = "$nvprofiledir\init.vim"
+
+function newsh {
+	Start-Process pwsh
+}
+
+function newsha {
+	Start-Process pwsh -Verb runAs
+}
+
+function admin($process) {
+	if (!$process) {
+		Write-Output "Missing process to run as admin. Use like 'admin `$processname'"
+	}
+	Start-Process $process -Verb runAs 
+}
+
+function symlink($link, $target) {
+	if (!$link || !$target) {
+		Write-Output "Missing link or target. Use like 'symlink `$link `$target'"
+		return
+	}
+    $cmd = "New-Item -ItemType SymbolicLink -Path '$link' -Target '$target'"
+    echo $cmd
+    Start-Process pwsh -Verb runAs -ArgumentList "-Command $cmd"
+}
